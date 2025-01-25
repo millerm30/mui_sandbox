900498d51705bca2cf5cc7508359e20a8027180c
millerm30 <millergm30@gmail.com>
2025-01-25 10:47:12 -0330

    update push container to mui

--- src/containers/PushContainer.tsx
+++ src/containers/PushContainer.tsx
@@ -1,26 +1,20 @@
 import AppleForm from '../components/AppleForm';
 import AndroidForm from '../components/AndroidForm';
-import { Row, Col, Grid } from 'antd';
 import { useLoadingContext } from '../hooks/useLoadingContext';
-
-const { useBreakpoint } = Grid;
+import { Grid2 } from '@mui/material';
 
 const PushContainer = () => {
   const { isLoading, isSubmitting } = useLoadingContext();
-  const screens = useBreakpoint();
 
   return (
-    <Row
-      gutter={[24, 24]}
-      style={{ flexDirection: screens.lg ? 'row' : 'column' }}
-    >
-      <Col span={screens.lg ? 12 : 24}>
+    <Grid2 container spacing={3}>
+      <Grid2 size={{ md: 6, xs: 12 }}>
         <AppleForm isLoading={isLoading} isSubmitting={isSubmitting} />
-      </Col>
-      <Col span={screens.lg ? 12 : 24}>
+      </Grid2>
+      <Grid2 size={{ md: 6, xs: 12 }}>
         <AndroidForm isLoading={isLoading} isSubmitting={isSubmitting} />
-      </Col>
-    </Row>
+      </Grid2>
+    </Grid2>
   );
 };
 
